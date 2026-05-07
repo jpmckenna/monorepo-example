d63433f64db8479cbc76663b2fc130bda354766f
John McKenna <jmckenna27@gmail.com>
2026-05-07 16:40:37 -0500

    cache files be gone

--- .vite-cache/storefront/deps/react.js
+++
@@ -1,5 +0,0 @@
-import {
-  require_react
-} from "./chunk-CIQ5EMC5.js";
-export default require_react();
-//# sourceMappingURL=react.js.map
--- .vite-cache/storefront/deps/react_jsx-runtime.js
+++
@@ -1,6 +0,0 @@
-import {
-  require_jsx_runtime
-} from "./chunk-QBCOO4EC.js";
-import "./chunk-CIQ5EMC5.js";
-export default require_jsx_runtime();
-//# sourceMappingURL=react_jsx-runtime.js.map
